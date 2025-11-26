db695c28d45ab081b0d609df70e823678141ba9b
tosik-14 <anton.mikhailouski@gmail.com>
2025-11-26 16:10:11 +0300

    feat: db is connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { MeetupModule } from "./meetups/meetup.module";
+
 
 @Module({
-  imports: [],
+  imports: [
+      MeetupModule,
+      TypeOrmModule.forRoot({
+        type: "postgres",
+        host: "localhost",
+        port: 5432,
+        username: "postgres",
+        password: "123",
+        database: "meetup_db",
+        entities: [__dirname + '\\**\\*.entity{.ts,.js}'],
+        synchronize: true,
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
