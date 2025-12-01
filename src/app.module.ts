27f039b17fc2da7d54ea6648926e557cf30f2fb2
tosik-14 <anton.mikhailouski@gmail.com>
2025-12-01 19:28:08 +0300

    feat: config service added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { MeetupModule } from "./meetups/meetup.module";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 
 @Module({
   imports: [
+      ConfigModule.forRoot({
+         isGlobal: true,
+         envFilePath: ".env",
+      }),
+
+      TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: (configService: ConfigService) => ({
+              type: "postgres",
+              host: configService.get<string>("DB_HOST"),
+              port: configService.get("DB_PORT"),
+              username: configService.get<string>("DB_USERNAME"),
+              password: configService.get<string>("DB_PASSWORD"),
+              database: configService.get<string>('DB_NAME'),
+              entities: [__dirname + '\\**\\*.entity{.ts,.js}'],
+              synchronize: configService.get<string>("NODE_ENV") === "development",
+          }),
+          inject: [ConfigService],
+      }),
       MeetupModule,
-      TypeOrmModule.forRoot({
-        type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: "postgres",
-        password: "123",
-        database: "meetup_db",
-        entities: [__dirname + '\\**\\*.entity{.ts,.js}'],
-        synchronize: true,
-      })
   ],
   controllers: [AppController],
   providers: [AppService],
