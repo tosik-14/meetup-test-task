8d8bf1ce8c23338da786856c3cf240598a0d8e16
tosik-14 <anton.mikhailouski@gmail.com>
2025-12-01 03:45:11 +0300

    feat: explanation for documentation added

--- src/meetups/dto/create-meetup.dto.ts
+++ src/meetups/dto/create-meetup.dto.ts
@@ -1,26 +1,52 @@
 import { MinLength, MaxLength, IsString, IsDateString, IsArray, ArrayMinSize, IsNotEmpty } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateMeetupDto {
+    @ApiProperty({
+        description: "Meetup title",
+        example: "Nestjs meetup",
+        minLength: 3,
+        maxLength: 20
+    })
     @MinLength(3)
     @MaxLength(20)
     @IsString()
     @IsNotEmpty()
     title: string;
 
+    @ApiProperty({
+        description: "Meetup description",
+        example: "Meetup for discuss Nestjs",
+        maxLength: 100
+    })
     @MaxLength(100)
     @IsString()
     @IsNotEmpty()
     description: string;
 
+    @ApiProperty({
+        description: "Meetup tags",
+        example: ["Nestjs", "Nodejs", "typescript"],
+        type: [String]
+    })
     @IsArray()
     @ArrayMinSize(1)
     @IsString({each: true})
     tags: string[];
 
+    @ApiProperty({
+        description: "Meetup date",
+        example: "2025-11-28T19:00:00Z"
+    })
     @IsDateString()
     @IsNotEmpty()
     date: string;
 
+    @ApiProperty({
+        description: "Meetup place",
+        example: "Minsk",
+        maxLength: 20
+    })
     @MaxLength(20)
     @IsString()
     @IsNotEmpty()
