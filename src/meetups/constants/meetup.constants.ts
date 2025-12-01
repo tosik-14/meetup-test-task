ea2e2fac2d407bea7990816e212d0c9a76fc8715
tosik-14 <anton.mikhailouski@gmail.com>
2025-12-01 03:28:58 +0300

    feat: constants for default value was added

---
+++ src/meetups/constants/meetup.constants.ts
@@ -0,0 +1,7 @@
+export const MEETUP_CONSTANT = {
+    SORT_FIELDS: ["title", "date", "place"],
+    DEFAULT_SORT_FIELD: "date",
+    DEFAULT_SORT_ORDER: "ASC",
+    DEFAULT_PAGE: 1,
+    DEFAULT_LIMIT: 3,
+}
