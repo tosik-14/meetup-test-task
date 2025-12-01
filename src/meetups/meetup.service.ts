d66add69008a59aa891de0444b81e9e2121de750
tosik-14 <anton.mikhailouski@gmail.com>
2025-12-01 03:27:43 +0300

    fix: findAll was updated for search/sort options

--- src/meetups/meetup.service.ts
+++ src/meetups/meetup.service.ts
@@ -4,6 +4,8 @@ import { Repository } from "typeorm";
 import { MeetupEntity } from "./entities/meetup.entity";
 import { CreateMeetupDto } from "./dto/create-meetup.dto";
 import { UpdateMeetupDto } from "./dto/update-meetup.dto";
+import { FindSortMeetupDto } from "./dto/find-sort-meetup.dto";
+import { MEETUP_CONSTANT } from "./constants/meetup.constants";
 
 @Injectable()
 export class MeetupService {
@@ -17,8 +19,40 @@ export class MeetupService {
         return await this.meetupRepository.save(meetup);
     }
 
-    async findAll(): Promise<MeetupEntity[]> {
-        return this.meetupRepository.find();
+    async findAll(query: FindSortMeetupDto): Promise<{ data: MeetupEntity[]; total: number }> {
+        const {
+            search,
+            tags,
+            sortBy = MEETUP_CONSTANT.DEFAULT_SORT_FIELD,
+            sortOrder = MEETUP_CONSTANT.DEFAULT_SORT_ORDER,
+            page = MEETUP_CONSTANT.DEFAULT_PAGE,
+            limit = MEETUP_CONSTANT.DEFAULT_LIMIT,
+        } = query
+
+        const queryBuilder = this.meetupRepository.createQueryBuilder("meetup");
+
+        if (search) {
+            queryBuilder.andWhere(
+                "(meetup.title ILIKE :search OR meetup.description ILIKE :search)",
+            { search: `%${search}%` }
+            );
+        }
+
+        if (tags && tags.length > 0) {
+            queryBuilder.andWhere(
+                "meetup.tags && :tags::text[]",
+                { tags }
+            );
+        }
+
+        const safeSortBy = MEETUP_CONSTANT.SORT_FIELDS.includes(sortBy) ? sortBy : MEETUP_CONSTANT.DEFAULT_SORT_FIELD;
+        queryBuilder.orderBy(`meetup.${safeSortBy}`, sortOrder);
+
+        const skip = (page - 1) * limit;
+        queryBuilder.skip(skip).take(limit);
+
+        const [data, total] = await queryBuilder.getManyAndCount();
+        return { data, total };
     }
 
     async findOne(id: number): Promise<MeetupEntity> {
