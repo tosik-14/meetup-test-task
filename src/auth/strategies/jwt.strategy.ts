d80405f318cdb6a820a10ba261c2114691e86b66
tosik-14 <anton.mikhailouski@gmail.com>
2025-12-02 00:21:45 +0300

    feat: jwt strategy created

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { ConfigService } from "@nestjs/config"
+import { AuthService } from "../auth.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
+    constructor(
+        private configService: ConfigService,
+        private authService: AuthService,
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.get<string>('JWT_SECRET')!,
+        });
+    }
+
+    async validate(tokenPayload: any) {
+        return this.authService.validateUserById(tokenPayload.id);
+    }
+}
