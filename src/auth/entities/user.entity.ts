ab3b2c5c54b193417ad749014347a153d79fd535
tosik-14 <anton.mikhailouski@gmail.com>
2025-12-01 17:09:13 +0300

    feat: user entity and roles created

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { MeetupEntity } from "../../meetups/entities/meetup.entity";
+import { ROLE } from "./user-role.enum";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({ default: ROLE.USER })
+    role: ROLE;
+
+    @OneToMany(() => MeetupEntity, (meetup) => meetup.organizer)
+    meetups: MeetupEntity[];
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    createdAt: Date;
+}
